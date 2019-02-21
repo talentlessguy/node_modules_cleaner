124c57474a882f05c15dbda7e0bc6cb092887c30
talentless_guy <pilll.PL22@gmail.com>
2019-02-21 13:26:01 +0300

    it works!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node_modules_cleaner
+
 Cleans node_modules from non code files.
+
+Demo uses `koa` package for demonstration.
---
+++ nm_clean.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const { unlinkSync, statSync } = require('fs')
+const readdir = require('recursive-readdir-sync')
+const { cyan, red } = require('chalk')
+const { log, error } = console
+
+const regex = /(LICENSE|Makefile)|(\.(md|doc|bashrc|eslintrc|babelrc))$/i
+
+const getSize = file => statSync(file).size / 1024
+
+if (process.argv.length <= 2) {
+    log(cyan(`
+    Usage:
+    nm_clean <project>
+    `))
+} else {
+    process.chdir(process.argv[2])
+    try {
+        const files = readdir('./node_modules')
+    } catch (e) {
+        error(
+            red(`
+        node_modules directory can't be found.
+        Please run npm i in your project.
+            `)
+        )
+    }
+    const trash = files.filter(file => regex.test(file))
+
+    // Calculate size of each file
+    let oldSize = 0
+    files.map(file => oldSize += getSize(file))
+    log(`Before: ${oldSize.toFixed(2)} Kb`)
+
+    // Clean from trash
+    trash.map(file => unlinkSync(file))
+
+    // Recalculate sizes
+    const cleaned = readdir('./node_modules')
+    let newSize = 0
+    cleaned.map(file => newSize += getSize(file))
+
+    log(`After: ${newSize.toFixed(2)} Kb`)
+}
